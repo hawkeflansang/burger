f852220441d537ecdcc629a927a8eeca61e25051
Hawke Lansang <hawkeflansang@gmail.com>
2019-11-05 19:33:44 -0500

    set up schema files and initiated db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Goat Cheese and Mushroom Stuffed Burger");
+INSERT INTO burgers (burger_name) VALUES ("Maple Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Triple Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Poutine Burger");
+INSERT INTO burgers (burger_name) VALUES ("Brisket Burger");

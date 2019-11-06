cd3dcad213245746035a3110ad229aed275a9e61
Hawke Lansang <hawkeflansang@gmail.com>
2019-11-05 19:52:10 -0500

    created sql syntax helpers and selectAll orm method

--- config/orm.js
+++ config/orm.js
@@ -38,3 +38,15 @@ function printQuestionMarks(num) {
     // translate array of strings to a single comma-separated string
     return arr.toString();
   }
+
+  var orm = {
+    selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+    }
+  };
